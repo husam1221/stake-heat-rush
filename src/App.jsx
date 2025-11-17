3cc55cee5b1ec6ba123835f2e521513998d0107b
husam1221 <husam.aljabre33@gmail.com>
2025-11-17 22:49:02 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -534,27 +534,27 @@ const App = () => {
 
   <div className="leaderboard-list">
     <div className="leaderboard-row">
-      <span className="rank">#1</span>
+      <span className="rank">1</span>
       <span className="addr">0xA1...F3b</span>
       <span className="amount">1.13 ETH</span>
     </div>
     <div className="leaderboard-row">
-      <span className="rank">#2</span>
+      <span className="rank">2</span>
       <span className="addr">0x9c...12e</span>
       <span className="amount">0.63 ETH</span>
     </div>
     <div className="leaderboard-row">
-      <span className="rank">#3</span>
+      <span className="rank">3</span>
       <span className="addr">0x7b...99a</span>
       <span className="amount">0.42 ETH</span>
     </div>
     <div className="leaderboard-row">
-      <span className="rank">#4</span>
+      <span className="rank">4</span>
       <span className="addr">0x5d...aa0</span>
       <span className="amount">0.41 ETH</span>
     </div>
     <div className="leaderboard-row">
-      <span className="rank">#5</span>
+      <span className="rank">5</span>
       <span className="addr">0x3e...bc1</span>
       <span className="amount">0.35 ETH</span>
     </div>
